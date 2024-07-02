657217a69238c143b078084be445489aceed5dca
“dzt2008” <dzt20081208@outlook.com>
2024-07-02 22:54:22 +0800

    Site updated: 2024-07-02 22:54:22

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1719931916330";
+const VERSION = "1719931931606";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
