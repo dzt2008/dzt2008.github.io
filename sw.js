5a052496d45121f8967195e5a29b5fe2cc5808a1
“dzt2008” <dzt20081208@outlook.com>
2024-09-16 22:36:00 +0800

    Site updated: 2024-09-16 22:35:59

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1726218838493";
+const VERSION = "1726497213466";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
