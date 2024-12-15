8e0bd5a61eda7588091b3d854040d3da8ef3cde4
冬涛觞雪 <121242192+dzt2008@users.noreply.github.com>
2024-12-15 15:48:30 +0800

    Site updated: 2024-12-15 15:48:30

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1734247971700";
+const VERSION = "1734248905820";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
